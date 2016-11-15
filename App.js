edc195b80763109ff71ff0912e0e587ce5f0bf28
virt_oscea@hotmail.com <virt_oscea@hotmail.com>
2016-11-15 16:37:45 -0600

    05-egghead-react-state-basics
    fifth lesson

--- App.js
+++ App.js
@@ -1,51 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-/*class App extends React.Component {
-    render() {
-        return React.createElement('h1', 
-            null,
-            'Hello guys')      
-    }
-}*/
-
-/*class App extends React.Component {
-    render() {
-        return <h1> Hello Guys </h1>
-    }
-}*/
-//const App = () => <h1>Hello </h1>
-
-
-/*class App extends React.Component {
-    render() {
-//Los elementos en el return, deberan ir dentro de un parentesis,
-// cuando sean mas de uno, en caso contrario, ponerlos en la misma 
-//linea que el return
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <b>Bold</b>
-            </div>
-        );      
-    }
-}*/
-
+import React from 'react'
 class App extends React.Component {
-    render() {
-        let txt = this.props.txt
-        return <h1>{txt}</h1>
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
     }
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+  render(){
+    return (
+      <div>
+      <input type="text"
+        onChange={this.update.bind(this)} />
+      <h1>{this.state.txt}</h1>
+      </div>
+    );
+  }
 }
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-App.defaultProps = {
-    txt: 'this is the default txt'
-}
-ReactDOM.render(
-    <App cat = {5} txt = "This is the props text"/>,
-    document.getElementById('app')
-);
+
 export default App
