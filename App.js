be37580fde02e76b7c27509b823a355df202a336
virt_oscea@hotmail.com <virt_oscea@hotmail.com>
2016-11-15 16:49:06 -0600

    06-egghead-react-owner-ownee-relationship

--- App.js
+++ App.js
@@ -2,10 +2,8 @@ import React from 'react'
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
+    this.state = { txt: ''}
+    this.update = this.update.bind(this)
   }
   update(e){
     this.setState({txt: e.target.value})
@@ -13,12 +11,22 @@ class App extends React.Component {
   render(){
     return (
       <div>
-      <input type="text"
-        onChange={this.update.bind(this)} />
-      <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update}/>
+        <Widget txt={this.state.txt} update={this.update}/>
+        <Widget txt={this.state.txt} update={this.update}/>
+        <Widget txt={this.state.txt} update={this.update}/>
       </div>
     );
   }
+}
+const Widget = (props) => {
+    return (
+      <div>
+      <input type="text"
+        onChange={props.update} />
+      <h1>{props.txt}</h1>
+      </div>
+    );
 }
 
 export default App
