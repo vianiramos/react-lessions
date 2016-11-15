f5e38adbae5043b4c8e65e38125009bc15dfbf6f
virt_oscea@hotmail.com <virt_oscea@hotmail.com>
2016-11-15 12:19:32 -0600

    03-egghead-react-the-render-method
    Third lession

--- App.js
+++ App.js
@@ -13,7 +13,21 @@ import React from 'react';
         return <h1> Hello Guys </h1>
     }
 }*/
-const App = () => <h1>Hello </h1> 
+//const App = () => <h1>Hello </h1>
 
+
+class App extends React.Component {
+    render() {
+//Los elementos en el return, deberan ir dentro de un parentesis,
+// cuando sean mas de uno, en caso contrario, ponerlos en la misma 
+//linea que el return
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <b>Bold</b>
+            </div>
+        );      
+    }
+}
 
 export default App
